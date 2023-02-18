b55719c948511c23504d2a291c708a9735052a38
Vadym Tsymbal <Vadym27@ukr.net>
2023-02-18 23:27:44 +0200

    Hw6

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import list_rates, query_list
+from currency.views import list_rates, query_list, status_code
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rate/list', list_rates),
-    path('contact/list', query_list)
+    path('contact/list', query_list),
+    path('status1/', status_code)
 ]
