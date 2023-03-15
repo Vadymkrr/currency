a7d503c552e1942c3d8adc50a20e01e1041c5eae
vadym <vadymtsymbal277@gmail.com>
2023-03-15 21:50:14 +0200

    Home work 13

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 
     path('auth/', include('django.contrib.auth.urls')),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('account/', include('account.urls')),
+
     path('__debug__/', include('debug_toolbar.urls')),
 
     path('', IndexView.as_view(template_name='index.html'), name='index'),
